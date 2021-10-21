b2368d1459cf1582ade5915fb553430d104f05a2
TimKorAnd <timkorand@gmail.com>
2021-10-21 16:51:01 +0300

    extract types and enums into separate files;

---
+++ constants/itemFields.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var ITEM_FIELDS;
+(function (ITEM_FIELDS) {
+    ITEM_FIELDS["CATEGORY"] = "category";
+    ITEM_FIELDS["DATA"] = "data";
+})(ITEM_FIELDS || (ITEM_FIELDS = {}));
+exports["default"] = ITEM_FIELDS;
