fc9a33418944ad9ea8219509cac0fb5605d92c4d
TimKorAnd <timkorand@gmail.com>
2021-10-21 17:39:31 +0300

    add test invalid input data type;

--- __tests__/main.test.js.ts
+++ __tests__/main.test.js.ts
@@ -35,15 +35,28 @@ const item4: Item = {
 
 const input: Input = [item1, item2, item3, item4];
 
+
 const output: Output = {
   cat_1: ["This item number 1 from category 1", "This item number 2 from category 1"],
   cat_2: ["This item number 1 from category 2", "This item number 2 from category 2"],
 }
-describe('lets test main', () => {
-  test('main return Promise', () => {
+describe('Test data transformation for valid input to specified output object', () => {
+  test('should successfully data resolved', () => {
     return main(input).then(data => {
       expect(data).toStrictEqual(output);
     })
+  });
+})
+
+describe('Tests throwing an specified errors for invalid input data ', () => {
+  test('should fail because input data is invalid', () => {
+    const invalidInput = [...input, 'invalidItem']
+    // @ts-expect-error
+    return main(invalidInput).then(data => {
+      throw 'should not resolve because input data is invalid';
+    }).catch(err => {
+      expect(err.message).toStrictEqual('item[itemFields_1.default.DATA] is not a function');
+    });
   });
 })
 
