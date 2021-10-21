175d65603fc62933b193e017fb12f80fedcfc73b
TimKorAnd <timkorand@gmail.com>
2021-10-21 17:21:41 +0300

    add jest ts dependency; add test main;

--- __tests__/main.test.js.ts
+++ __tests__/main.test.js.ts
@@ -1,15 +1,49 @@
-import * as main from "../main";
+import { main, getOutput, getInputsWithResolvedData } from "../main";
 import { Item, Input, Output, ItemWithResolvedData } from '../main.d';
+
 import ITEM_WITH_RESOLVED_DATA_FIELDS from "../constants/itemWithResolvedDataFields";
 import ITEM_FIELDS from "../constants/itemFields";
 
+
 const item1: Item = {
+  category: "cat_1",
+  data: () => new Promise((resolve) => {
+    setTimeout(() => resolve("This item number 1 from category 1"), 1000)
+  }),
+};
+
+const item2: Item = {
+  category: "cat_1",
+  data: () => new Promise((resolve) => {
+    setTimeout(() => resolve("This item number 2 from category 1"), 1000)
+  }),
+};
+
+const item3: Item = {
+  category: "cat_2",
+  data: () => new Promise((resolve) => {
+    setTimeout(() => resolve("This item number 1 from category 2"), 1000)
+  }),
+};
+
+const item4: Item = {
   category: "cat_2",
   data: () => new Promise((resolve) => {
     setTimeout(() => resolve("This item number 2 from category 2"), 1000)
   }),
 };
 
-const input: Input = [item1, ];
+const input: Input = [item1, item2, item3, item4];
 
-main.main(input);
+const output: Output = {
+  cat_1: ["This item number 1 from category 1", "This item number 2 from category 1"],
+  cat_2: ["This item number 1 from category 2", "This item number 2 from category 2"],
+}
+describe('lets test main', () => {
+  test('main return Promise', () => {
+    return main(input).then(data => {
+      expect(data).toStrictEqual(output);
+    })
+  });
+})
+
--- main.ts
+++ main.ts
@@ -1,42 +1,7 @@
-import { Item, Input, Output, ItemWithResolvedData } from './main.d';
+import { Input, Output, ItemWithResolvedData } from './main.d';
 import ITEM_WITH_RESOLVED_DATA_FIELDS from "./constants/itemWithResolvedDataFields";
 import ITEM_FIELDS from "./constants/itemFields";
 
-
-const item1: Item = {
-  category: "cat_1",
-  data: () => new Promise((resolve) => {
-    setTimeout(() => resolve("This item number 1 from category 1"), 1000)
-  }),
-};
-
-const item2: Item = {
-  category: "cat_1",
-  data: () => new Promise((resolve) => {
-    setTimeout(() => resolve("This item number 2 from category 1"), 1000)
-  }),
-};
-
-const item3: Item = {
-  category: "cat_2",
-  data: () => new Promise((resolve) => {
-    setTimeout(() => resolve("This item number 1 from category 2"), 1000)
-  }),
-};
-
-const item4: Item = {
-  category: "cat_2",
-  data: () => new Promise((resolve) => {
-    setTimeout(() => resolve("This item number 2 from category 2"), 1000)
-  }),
-};
-
-const input: Input = [item1, item2, item3, item4];
-
-const output: Output = {
-  cat_1: ["This item number 1 from category 1", "This item number 2 from category 1"],
-  cat_2: ["This item number 1 from category 2", "This item number 2 from category 2"],
-}
 
 export const getInputsWithResolvedData = async (input: Input): Promise<ItemWithResolvedData[]> => {
   return await Promise.all(input.map(async (item) => ({
@@ -59,8 +24,5 @@ export const main = async (input: Input): Promise<Output> => {
   return getOutput(inputsWithResolvedData);
 }
 
-
-console.table(output);
-main(input).then(output => console.table(output));
 
 
